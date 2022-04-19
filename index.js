1e7baa6ee480095e228ad0e56998c008c2b6504d
Marcos <marcossuarez2004@gmail.com>
2022-04-19 17:41:15 -0300

    Adição da rota de mensagem

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 
 app.get('/',function(req,res){
     res.send("Olá Mundo")
+})
+
+app.get('/mensagem',function(req,res){
+    res.send("Está mensagem é automática!")
 })
 
 app.listen(3000,function(){
